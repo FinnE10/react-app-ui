37e8f537e656022f07b8baae67a020a88d9518e3
FinnE10 <FinnE10@users.noreply.github.com>
2019-09-05 17:54:19 +0100

    Ammending test to use shallow

--- tests/components/app.test.tsx
+++ tests/components/app.test.tsx
@@ -1,10 +1,10 @@
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import App from "../../src/components/App";
 import * as React from "react";
 
 describe("Should render app", () => {
-    it("Render app", () => {
-        const wrapper = mount(<App/>)
+    it("Render app and find home", () => {
+        const wrapper = shallow(<App/>)
         const app = wrapper.find("Home");
 
         expect(app).toHaveLength(1);

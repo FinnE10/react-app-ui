f261a523ab038f3c1b4f83923a3ead53088662c0
FinnE10 <callum.finn@btinternet.com>
2019-12-15 10:37:53 +0000

    Cleaing up config

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,23 @@
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require("path");
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html"
+});
+
+const cssPlugin = new MiniCssExtractPlugin({
+    // Options similar to the same options in webpackOptions.output
+    // all options are optional
+    filename: '[name].css',
+    chunkFilename: '[id].css',
+    ignoreOrder: false // Enable to remove warnings about conflicting order
 });
 
 module.exports = {
     entry: "./src/index.tsx",
     output: {
         path: path.resolve(__dirname, "dist"),
-        
         filename: "bundle.js"
     },
     resolve: {
@@ -26,30 +33,21 @@ module.exports = {
             }, 
             {
                 test: /\.css$/,
-                use: [
-                  {
+                use: [{
                     loader: MiniCssExtractPlugin.loader,
                     options: {
-                      // you can specify a publicPath here
-                      // by default it uses publicPath in webpackOptions.output
-                      publicPath: '../',
-                      hmr: process.env.NODE_ENV === 'development',
-                    },
-                  },
-                  'css-loader',
-                ],
-            },
+                        // you can specify a publicPath here
+                        // by default it uses publicPath in webpackOptions.output
+                        publicPath: '../',
+                        hmr: process.env.NODE_ENV === 'development'
+                    }},
+                    'css-loader'
+                ]
+            }
         ]
     },
-
     plugins: [
         htmlPlugin,
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
-            filename: '[name].css',
-            chunkFilename: '[id].css',
-            ignoreOrder: false, // Enable to remove warnings about conflicting order
-        })
+        cssPlugin
     ]
 }

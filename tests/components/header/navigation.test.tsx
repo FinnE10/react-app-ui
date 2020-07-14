3471281b8bf4ac80c84b7b07acc96aefb7b50758
Finn-E10 <callum.finn@btinternet.com>
2020-07-14 18:15:01 +0100

    Refactoring tests

--- tests/components/header/navigation.test.tsx
+++ tests/components/header/navigation.test.tsx
@@ -3,17 +3,37 @@ import Navigation from "../../../src/components/header/nagivation";
 import * as React from "react";
 
 describe("Test Navigation component", () =>{
-    it("Render Navigation component and finds all components", () => {
+    describe("Render Navigation component and finds all components", () => {
         const wrapper = mount(<Navigation />);
-
-        const navbar = wrapper.find("Navbar");
-        const navs = wrapper.find("Nav");
-        const navbarBrand = wrapper.find(".navbar-brand");
-        const icons = wrapper.find("FontAwesomeIcon");
-
-        expect(navbar).toHaveLength(1);
-        expect(navs).toHaveLength(2);
-        expect(navbarBrand).toHaveLength(1);
-        expect(icons).toHaveLength(3);
+        it("Find Navbar parent comoponent", () => {
+            const navbar = wrapper.find("Navbar");
+            expect(navbar).toHaveLength(1);
+        });
+        it("Find Nav child components", () => {
+            const navs = wrapper.find("Nav");
+            expect(navs).toHaveLength(2);
+        });
+        it("Find NavbarBrand", () => {
+            const navbarBrand = wrapper.find(".navbar-brand");
+            expect(navbarBrand).toHaveLength(1);
+        });
     });
+    
+    describe("Nav links", () => {
+        const wrapper = mount(<Navigation />);
+        it("Instagram icon is found", () => {
+            
+            const instagramLink = wrapper.find(".fa-instagram");
+            expect(instagramLink).toHaveLength(1);
+        });
+        it("Twitter icon is found", () => {
+            const twitterLink = wrapper.find(".fa-twitter");
+            expect(twitterLink).toHaveLength(1);
+        });
+        it("GitHub icon is found", () => {
+            const githubLink = wrapper.find(".fa-github");
+            expect(githubLink).toHaveLength(1);
+        });
+    });
+    
 });

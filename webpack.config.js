3ca6d369ff8f0d96906ece5d89be230cdd490d0e
FinnE10 <callum.finn@btinternet.com>
2021-12-04 15:19:40 +0000

    Removing unused varaible

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const { merge } = require('webpack-merge');
-const util = require('util')
 
 const commonConfig = require("./webpack.common");
 const devConfig = require("./webpack.dev");
 
 module.exports = env => {
-    // throw new Error(util.inspect(env, {showHidden: false, depth: null}));
     if (env.development == true) {
         return merge(commonConfig, devConfig);
     } else if (env.production == true) {
